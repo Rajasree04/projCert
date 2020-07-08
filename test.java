40dd470810d4ffbf58135dd467b859ea0113a98e
Rajasree04 <37287901+Rajasree04@users.noreply.github.com>
2020-07-08 04:22:23 -0700

    Update test.java

--- test.java
+++ test.java
@@ -8,11 +8,11 @@ public class NewTest {
 
 	//System.setProperty("webdriver.chrome.driver","chromedriver.exe");
 	WebDriver driver = new ChromeDriver();
-  driver.get("ec2-3-15-148-238.us-east-2.compute.amazonaws.com:8081/");
+  driver.get("http://ec2-3-15-148-238.us-east-2.compute.amazonaws.com:8082/");
   driver.findElement(By.linkText("Home")).click();
   String at=driver.getTitle();
   String et="home";
-  //driver.close();
+  driver.close();
   if(at.equalsIgnoreCase(et))
   {
     System.out.println("Test Successfull");
