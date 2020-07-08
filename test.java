815260f7cce443199209630d797bc2c30000b90a
Rajasree04 <37287901+Rajasree04@users.noreply.github.com>
2020-07-08 04:17:09 -0700

    Update test.java

--- test.java
+++ test.java
@@ -6,13 +6,13 @@ import org.openqa.selenium.WebElement;
 
 public class NewTest {
 
-	System.setProperty("webdriver.chrome.driver","chromedriver.exe");
+	//System.setProperty("webdriver.chrome.driver","chromedriver.exe");
 	WebDriver driver = new ChromeDriver();
   driver.get("ec2-3-15-148-238.us-east-2.compute.amazonaws.com:8081/");
   driver.findElement(By.linkText("Home")).click();
   String at=driver.getTitle();
   String et="home";
-  driver.close();
+  //driver.close();
   if(at.equalsIgnoreCase(et))
   {
     System.out.println("Test Successfull");
